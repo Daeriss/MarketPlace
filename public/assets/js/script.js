37920706195beedc813bc91727e45f17dcbe67bc
Maeva Zulmea <maeva.zulmea@gmail.com>
2021-01-14 14:44:42 +0100

    recherche shop ok

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,12 +1,12 @@
-document.addEventListener("DOMContentLoaded", function setLocalStorage() {
+// document.addEventListener("DOMContentLoaded", function setLocalStorage() {
     
-    if (typeof Storage !== "undefined") {
+//     if (typeof Storage !== "undefined") {
 
-      dataDistrict = document.getElementById("district-Id");
-      district = dataDistrict.dataset.district;
-      localStorage.setItem("district", district);
-      document.getElementById("district-Id").innerHTML = localStorage.getItem("district");
-    }
-  },
-  false
-);
+//       dataDistrict = document.getElementById("district-Id");
+//       district = dataDistrict.dataset.district;
+//       localStorage.setItem("district", district);
+//       document.getElementById("district-Id").innerHTML = localStorage.getItem("district");
+//     }
+//   },
+//   false
+// );
--- src/Controller/MarketController.php
+++ src/Controller/MarketController.php
@@ -3,13 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\DistrictType;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\ShopRepository;
 
 class MarketController extends AbstractController
 {
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
     /**
      * @Route("/market", name="market")
      */
@@ -25,6 +33,7 @@ class MarketController extends AbstractController
      */
     public function accueil(Request $request): Response
     {
+        
 
         $form = $this->createForm(DistrictType::class);
         $form->handleRequest($request);
@@ -32,12 +41,13 @@ class MarketController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
 
         $district = $form->get('Code_Postale')->getData();
-        
+        $this->session->set('district', $district);
         dump($district);
+        return $this->redirectToRoute('shops');
+
         }
         return $this->render('market/index.html.twig', [
             'form' => $form->createView(),
-            'district' => $district ?? "13001"
         ]);
     }
 
@@ -47,5 +57,19 @@ class MarketController extends AbstractController
     public function accueilshopkeeper()
     {
         return $this->render('market/indexshopKeeper.html.twig');
+    }
+
+    /**
+     * @Route("/shops", name="shops")
+     */
+    public function shops(ShopRepository $shopRepository)
+    {
+        $listeShops = $shopRepository->findBy(
+            ['adress' => $this->session->get('district')],
+            []
+        );
+        
+        return $this->render("market/shops.html.twig", [ 
+            'shops' => $listeShops]);
     }
 }
