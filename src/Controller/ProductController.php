8981eaab7ac2ee7b692713d213ac6bb453d96eff
Maeva Zulmea <maeva.zulmea@gmail.com>
2021-01-15 12:06:53 +0100

    image produits

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -9,6 +9,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 /**
  * @Route("/member/product")
@@ -28,7 +30,7 @@ class ProductController extends AbstractController
     /**
      * @Route("/new", name="product_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request,  SluggerInterface $slugger): Response
     {
         $product = new Product();
         $user = $this->getUser();
@@ -43,6 +45,26 @@ class ProductController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $img = $form->get('img')->getData();
+            if ($img) {
+                $originalFilename = pathinfo($img->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $img->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $img->move(
+                        $this->getParameter('photos_directory'),        // NE PAS OUBLIER DE CREER LE DOSSIER
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                   
+                }
+
+                $product->setImg($newFilename);       // ON ENREGISTRE LE NOM DU FICHIER
+            }
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($product);
             $entityManager->flush();
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -8,6 +8,8 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use App\Entity\Shop;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 
 class ProductType extends AbstractType
 {
@@ -17,7 +19,20 @@ class ProductType extends AbstractType
             ->add('name')
             ->add('description')
             ->add('price')
-            ->add('img')
+            ->add('img', FileType::class, [
+                'label' => 'photo à uploader',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '10240k',      // 10 Mo ?
+                        'mimeTypes' => [
+                            'image/*',              // SEULEMENT DES IMAGES
+                        ],
+                        'mimeTypesMessage' => 'SEULEMENT UN FICHIER IMAGE...',
+                    ])
+                ],
+            ])
             // ->add('shop', EntityType::class, [ "class" => Shop::class, "choice_label" => "name" ])
         ;
     }
