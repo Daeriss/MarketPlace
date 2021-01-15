8abbe68e259efb3ec5c2bc66534f2ce551fdbaad
Maeva Zulmea <maeva.zulmea@gmail.com>
2021-01-15 09:51:41 +0100

    checkbox not required

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,7 +20,8 @@ class RegistrationFormType extends AbstractType
             ->add('email')
             ->add('name')
             ->add ('shopKeeper', CheckboxType::class, [
-                'mapped' => false
+                'mapped' => false,
+                'required' => false
             ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
