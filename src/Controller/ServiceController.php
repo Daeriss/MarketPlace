8bde8f65b1b034651d9106ad11ee2ce69edbeb3a
Maeva Zulmea <maeva.zulmea@gmail.com>
2021-01-21 11:15:45 +0100

    debut calendar

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\CalendarRepository;
 
 class ServiceController extends AbstractController
 {
@@ -21,11 +22,26 @@ class ServiceController extends AbstractController
     /**
      * @Route("/service/appointment", name="appointment")
      */
-    public function appointment(): Response
+    public function appointment(CalendarRepository $calendarRepository): Response
     {
-        return $this->render('service/appointment.html.twig', [
-            'controller_name' => 'ServiceController',
-        ]);
+        $events = $calendarRepository->findAll();
+        $rdvs = [];
+
+        foreach ($events as $event){
+            $rdvs[] = [
+                'id' => $event->getId(),
+                'start' => $event->getStart()->format('Y-m-d H:i:s'),
+                'end' => $event->getEnd()->format('Y-m-d H:i:s'),
+                'title' => $event->getTitle(),
+                'description' => $event->getDescription(),
+                'backgroundColor' => $event->getBackgroundColor(),
+                'allDay' => $event->getAllDay(),
+            ];
+        }
+
+        $data = json_encode($rdvs);
+
+        return $this->render('service/appointment.html.twig', compact('data'));
     }
 
     /**
